0de28548e0d15fea4b27c7a6c1c772c52a365575
ciro <ciroandreastrazzullo@icloud.com>
2026-01-24 01:33:09 +0100

    feat: show homepage calendar

--- src/app/(authenticated)/(home)/page.tsx
+++ src/app/(authenticated)/(home)/page.tsx
@@ -1,5 +1,52 @@
-export default function Home(){
+"use client";
+import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import {CalendarGrid} from "@/components/home/calendar-grid";
+
+export default function Home() {
+    const router = useRouter();
+
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+
     return (
-        <h1>HomePage</h1>
+        <div className="min-h-screen  ">
+             <div className="container mx-auto flex flex-col h-screen px-4 sm:px-6 py-6">
+                <div className="flex items-center justify-between mb-6">
+                    <h1 className="text-3xl font-light text-foreground flex-1">
+                        Il tuo calendario
+                    </h1>
+
+                    <Button
+                        variant="secondary"
+                        onClick={() => router.push('/createEvent')}
+                        className="min-w-[100px] font-light shadow-xs"
+                    >
+                        <span className="ml-2 mr-2 text-white">+</span>
+                        <span className="flex-1 text-white">Nuovo evento</span>
+                    </Button>
+
+
+                </div>
+
+                 <div className="flex flex-1 flex-col md:flex-row gap-6 min-h-0">
+                     <div className="w-full md:w-8/12  rounded-lg shadow overflow-hidden flex-1 min-h-0">
+                        <CalendarGrid
+                            className="p-5"
+                            selectedDate={selectedDate}
+                            onSelectDate={(date: Date) => setSelectedDate(date)}
+                        />
+                    </div>
+
+                     <aside className="w-full md:w-4/12 sm:pl-4">
+                        <div className="bg-white rounded-lg shadow p-4 h-full min-h-0">
+                            <h2 className="font-medium mb-2">Giorno selezionato:</h2>
+
+                        </div>
+                    </aside>
+
+                </div>
+            </div>
+        </div>
     )
 }
